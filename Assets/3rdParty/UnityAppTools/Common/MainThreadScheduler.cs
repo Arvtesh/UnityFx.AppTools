e7308113e35549384c79fb4e8285d6d6f5b007ac
Alexander Bogarsukov <a.bogarsukov@program-ace.com.ua>
2018-01-30 20:32:39 +0200

    added main thread scheduler pilot

---
+++ Assets/3rdParty/UnityAppTools/Common/MainThreadScheduler.cs
@@ -0,0 +1,154 @@
+﻿// Copyright (c) Alexander Bogarsukov.
+// Licensed under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using UnityEngine;
+
+namespace UnityAppTools
+{
+	/// <summary>
+	/// Main thread scheduler that can be used make sure the code is executed on the main thread.
+	/// The implementation setups a <see cref="SynchronizationContext"/> and attaches it to the main thread.
+	/// Do not use this on Unity 2017+ with .NET 4.6 profile (there is a native Unity <see cref="SynchronizationContext"/>
+	/// attached to the main thread already).
+	/// </summary>
+	/// <seealso cref="UnitySynchronizationContext"/>
+	public sealed class MainThreadScheduler : MonoBehaviour, IAsyncScheduler
+	{
+		#region data
+
+		private struct ActionData
+		{
+			public SendOrPostCallback Action;
+			public object State;
+		}
+
+		private UnitySynchronizationContext _context;
+		private int _mainThreadId;
+		private Queue<ActionData> _actionQueue = new Queue<ActionData>();
+
+		#endregion
+
+		#region interface
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="d"></param>
+		/// <param name="asyncState"></param>
+		/// <returns></returns>
+		public IAsyncResult BeginInvoke(SendOrPostCallback d, object asyncState)
+		{
+			if (d == null)
+			{
+				throw new ArgumentNullException("d");
+			}
+
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="asyncResult"></param>
+		public void EndInvoke(IAsyncResult asyncResult)
+		{
+			if (asyncResult == null)
+			{
+				throw new ArgumentNullException("asyncResult");
+			}
+
+			throw new NotImplementedException();
+		}
+
+		#endregion
+
+		#region MonoBehaviour
+
+		private void Awake()
+		{
+			var currentContext = SynchronizationContext.Current;
+
+			if (currentContext != null)
+			{
+				throw new InvalidOperationException("SynchronizationContext instance is already set.");
+			}
+
+			var context = new UnitySynchronizationContext(this);
+			SynchronizationContext.SetSynchronizationContext(context);
+
+			_context = context;
+			_mainThreadId = Thread.CurrentThread.ManagedThreadId;
+		}
+
+		private void OnDestroy()
+		{
+			if (_context == SynchronizationContext.Current)
+			{
+				SynchronizationContext.SetSynchronizationContext(null);
+			}
+
+			_context = null;
+		}
+
+		private void Update()
+		{
+			if (_actionQueue.Count > 0)
+			{
+				lock (_actionQueue)
+				{
+					while (_actionQueue.Count > 0)
+					{
+						var data = _actionQueue.Dequeue();
+						data.Action.Invoke(data.State);
+					}
+				}
+			}
+		}
+
+		#endregion
+
+		#region IAsyncScheduler
+
+		/// <inheritdoc/>
+		public void Send(SendOrPostCallback d, object state)
+		{
+			if (d == null)
+			{
+				throw new ArgumentNullException("d");
+			}
+
+			if (_mainThreadId == Thread.CurrentThread.ManagedThreadId)
+			{
+				d.Invoke(state);
+			}
+			else
+			{
+				var asyncResult = BeginInvoke(d, state);
+				EndInvoke(asyncResult);
+			}
+		}
+
+		/// <inheritdoc/>
+		public void Post(SendOrPostCallback d, object state)
+		{
+			if (d == null)
+			{
+				throw new ArgumentNullException("d");
+			}
+
+			lock (_actionQueue)
+			{
+				_actionQueue.Enqueue(new ActionData() { Action = d, State = state });
+			}
+		}
+
+		#endregion
+
+		#region implementation
+		#endregion
+	}
+}
---
+++ Assets/3rdParty/UnityAppTools/DotnetExtensions/UnitySynchronizationContext.cs
@@ -0,0 +1,86 @@
+﻿// Copyright (c) Alexander Bogarsukov.
+// Licensed under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System;
+using System.Threading;
+
+namespace UnityAppTools
+{
+	/// <summary>
+	/// Implementation of <see cref="SynchronizationContext"/> for Unity. This class is a helper for <see cref="MainThreadScheduler"/>;
+	/// do not use unless absolutely nessesary.
+	/// </summary>
+	/// <seealso cref="IAsyncScheduler"/>
+	public sealed class UnitySynchronizationContext : SynchronizationContext, IDisposable
+	{
+		#region data
+
+		private IAsyncScheduler _scheduler;
+
+		#endregion
+
+		#region interface
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="AsyncResult"/> class.
+		/// </summary>
+		public UnitySynchronizationContext(IAsyncScheduler scheduler)
+		{
+			if (scheduler == null)
+			{
+				throw new ArgumentNullException("scheduler");
+			}
+
+			_scheduler = scheduler;
+		}
+
+		#endregion
+
+		#region SynchronizationContext
+
+		/// <inheritdoc/>
+		public override SynchronizationContext CreateCopy()
+		{
+			ThrowifDisposed();
+			return new UnitySynchronizationContext(_scheduler);
+		}
+
+		/// <inheritdoc/>
+		public override void Send(SendOrPostCallback d, object state)
+		{
+			ThrowifDisposed();
+			_scheduler.Send(d, state);
+		}
+
+		/// <inheritdoc/>
+		public override void Post(SendOrPostCallback d, object state)
+		{
+			ThrowifDisposed();
+			_scheduler.Post(d, state);
+		}
+
+		#endregion
+
+		#region IDisposable
+
+		/// <inheritdoc/>
+		public void Dispose()
+		{
+			_scheduler = null;
+		}
+
+		#endregion
+
+		#region implementation
+
+		private void ThrowifDisposed()
+		{
+			if (_scheduler == null)
+			{
+				throw new ObjectDisposedException(GetType().Name);
+			}
+		}
+
+		#endregion
+	}
+}
